4252a5e9279210aacde943dcc1955b8314c4321a
Sergio Arias <s-andres140@hotmail.com>
2025-02-14 22:41:20 -0500

    feat: :sparkles: add cors to the express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 // Configura Express
 import express from "express";
+import cors from 'cors';
 import router from "./routes";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
