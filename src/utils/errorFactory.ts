071a9aab68dec57ea72956fc1cafe1e0c47726f1
Sergio Arias <s-andres140@hotmail.com>
2025-03-04 17:03:42 -0500

    refactor: :hammer: update BusinessError class to add personalized statusCode

--- src/utils/errorFactory.ts
+++ src/utils/errorFactory.ts
@@ -1,8 +1,10 @@
 const createErrorFactory = function (name: string) {
     return class BusinessError extends Error {
-        constructor(message: string) {
+        statusCode: number;
+        constructor(message: string, statusCode: number =500) {
             super(message);
             this.name = name;
+            this.statusCode = statusCode;
         }
     };
 };
