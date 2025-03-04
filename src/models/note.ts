84cf0c89896a587f9c02c62e37d24a1a4665be82
Sergio Arias <s-andres140@hotmail.com>
2025-03-04 17:19:15 -0500

    refactor: :hammer: update Note model to make 'visible' property optional and improve formatting

--- src/models/note.ts
+++ src/models/note.ts
@@ -4,52 +4,52 @@ import { NoteAttributes, NoteCreationAttributes } from '../types/types';
 import { User } from './user';
 
 class Note extends Model<NoteAttributes, NoteCreationAttributes> implements NoteAttributes {
-  public id!: number;
-  public title!: string;
-  public content!: string;
-  public visible!: boolean;
-  public userId!: number;
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
+    public id!: number;
+    public title!: string;
+    public content!: string;
+    public visible?: boolean;
+    public userId!: number;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
 }
 
 Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        visible: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+        }
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    visible: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: 'id',
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
+    {
+        sequelize,
+        tableName: 'Notes',
+        modelName: 'Note',
+        timestamps: true,
     }
-  },
-  {
-    sequelize,
-    tableName: 'Notes',
-    modelName: 'Note',
-    timestamps: true,
-  }
 );
 
 export { Note };
